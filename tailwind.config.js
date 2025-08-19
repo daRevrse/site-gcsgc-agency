0aaf843611ec63ae30511fa66949deed473940ec
mr_gass <95222903+daRevrse@users.noreply.github.com>
2025-08-19 09:19:44 +0000

    site v2

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "gcsgc-blue": "#00aab3",
+        "gcsgc-dark": "#002e60",
+      },
+      fontFamily: {
+        sans: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "sans-serif",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
